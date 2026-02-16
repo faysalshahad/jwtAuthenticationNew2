9ed3138a40018ac26e74b9d214c314587c55c6eb
faysalshahad <faysalshahad@gmail.com>
2026-02-16 10:24:38 +0600

    2/16/2026 10:25 AM

--- springSecurityJwt3/src/main/java/com/springsecurityjwt3/springSecurityJwt3/config/SecurityConfig.java
+++ springSecurityJwt3/src/main/java/com/springsecurityjwt3/springSecurityJwt3/config/SecurityConfig.java
@@ -91,7 +91,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
